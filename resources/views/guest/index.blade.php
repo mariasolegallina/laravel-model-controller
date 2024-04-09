be5727ef820fbd48458bbcced7f25e619af7ac63
mariasolegallina <gallinamariasole@gmail.com>
2024-04-09 23:46:04 +0200

    print in index movie title&original title + style with Bootstrap

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,13 +2,17 @@
 
 @section('content')
 <section class="movies">
-    <div class="container">
-        <div class="row">
+    <div class="container my-3">
+        <div class="row row-cols-3 row-gap-3">
             @foreach ($movies as $movie)
-            <div class="col-3">
-                <h2>{{ $movie->title }}</h2>
+            <div class="col d-flex align-items-stretch">
+                <div class="card card-body ">
+                    <h2 class="fs-4 fw-bold">{{ $movie->title }}</h2>
+                    <h2 class="fs-6 fst-italic">{{ $movie->original_title }}</h2>
+                </div>
             </div>
             @endforeach
+
         </div>
     </div>
     </div>
